2e90d0b4baaa3c3d189c68fbfb05dbe039239295
manab <jyotimanab@gmail.com>
2024-10-18 15:17:35 +0530

    script to create directory from names with script

---
+++ createDir/README.md
@@ -0,0 +1,1 @@
+#script to create diretory from name
--- createDir/create-folder from file names.sh
+++ createDir/create-folder from file names.sh
@@ -1,1 +1,12 @@
+if [ $# -eq 0 ]; then
+        echo "usages: $0 <file>"
+        exit 1
+fi
+while IFS= read -r line; do
+        trimed_name=$(echo "$line" | xargs)
 
+        if [ -n $trimed_name ]; then
+                mkdir -p "/tmp/$trimed_name"
+                echo "Created Directory: $trimed_name"
+        fi
+done < "$1"
